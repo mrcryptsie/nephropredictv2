7a248b045b61f6d75d405f859bcc36a16456374b
mrcryptsie <richardkeke15@gmail.com>
2025-04-10 17:42:56 +0100

    dfdfddf

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -27,7 +22,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
