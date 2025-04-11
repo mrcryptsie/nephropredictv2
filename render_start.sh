21b839e173fefc1565eb404e94c826dcd6972df8
mrcryptsie <richardkeke15@gmail.com>
2025-04-11 09:40:03 +0100

    fdfd

--- render_start.sh
+++ render_start.sh
@@ -23,7 +23,7 @@ fi
 echo "=== Installation des dépendances Python ==="
 cd "$FASTAPI_DIR" || exit 1
 echo "Installation des requirements..."
-pip install -r requirements.txt
+pip install -r production-requirements.txt
 
 # Démarrage du frontend
 echo "=== Démarrage du Frontend ==="
